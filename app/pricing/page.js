cfb8de96cf067c9e1bf33cabcb5e7f42af700559
kumarsuneel162@gmail.com <kumarsuneel162@gmail.com>
2023-10-02 11:33:47 +0530

    first commit

---
+++ app/pricing/page.js
@@ -0,0 +1,12 @@
+import Pricing from "@/componets/homepage/Pricing";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Pricing />
+    </>
+  );
+};
+
+export default page;
